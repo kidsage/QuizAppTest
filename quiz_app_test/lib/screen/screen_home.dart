13e9d344f19fc4ac7eda81dbbfb26f5998b12813
kidsage <ruhyun92@gmail.com>
2023-01-05 19:36:40 +0900

    Feat: 퀴즈 모델 및 퀴즈 풀기화면 생성
    - screen_quiz.dart 생성(미완)
    - screen_home.dart 버튼 업데이트
    - widget_candidate.dart 생성
    - 기타 추가적인 생성작업 및 모듈 추가

--- quiz_app_test/lib/screen/screen_home.dart
+++ quiz_app_test/lib/screen/screen_home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app_test/model/model_quiz.dart';
+import 'package:quiz_app_test/screen/screen_quiz.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -6,6 +8,25 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  // quiz 더미 데이터 생성
+  List<Quiz> quizs = [
+    Quiz.fromMap({
+      'title': 'test',
+      'candidates': ['a', 'b', 'c', 'd'],
+      'answer': 0,
+    }),
+    Quiz.fromMap({
+      'title': 'test',
+      'candidates': ['a', 'b', 'c', 'd'],
+      'answer': 0,
+    }),
+    Quiz.fromMap({
+      'title': 'test',
+      'candidates': ['a', 'b', 'c', 'd'],
+      'answer': 0,
+    }),
+  ];
+
   @override
   Widget build(BuildContext context) {
     Size screenSize =
@@ -49,7 +70,7 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
             _buildstep(width, '1. 랜덤으로 나오는 퀴즈 3개를 풀어보세요.'),
             _buildstep(width, '2. 문제를 잘 읽고 정답을 고른 뒤\n다음 문제 버튼을 눌러주세요.'),
-            _buildstep(width, '3. 만점을 향해 도전해보세요!.'),
+            _buildstep(width, '3. 만점을 향해 도전해보세요!?.'),
             Padding(
               padding: EdgeInsets.all(width * 0.048),
             ),
@@ -62,15 +83,23 @@ class _HomeScreenState extends State<HomeScreen> {
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(10),
                   ),
-                  child: RaisedButton(
-                    child(
-                      Text(
-                        '지금 퀴즈 풀기',
-                        style: TextStyle(color: Colors.white),
-                      ),
-                      color: Colors.deepPurple,
-                      onPressed: () {},
+                  // RaisedButton 대신 ElevatedButton으로 대체
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => QuizScreen(
+                            quizs: quizs,
+                          ),
+                        ),
+                      );
+                    },
+                    child: const Text(
+                      '지금 퀴즈 풀기',
+                      style: TextStyle(color: Colors.white),
                     ),
+                    // style: ElevatedButton.styleFrom(primary: Colors.white),
                   ),
                 ),
               ),
