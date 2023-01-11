a71bc8ba8020fb5a30e1b04e23504d1f5d5abf78
kidsage <ruhyun92@gmail.com>
2023-01-11 22:55:36 +0900

    Feat: quiz backend 기초 세팅 완료
    - models, serializers, views, urls, admin 기본 토대 작성 완료

--- quiz_app_test_backend/quiz/views.py
+++ quiz_app_test_backend/quiz/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+import random
+from rest_framework.response import Response
+# 함수형 view 사용 시 쓰는 decorator
+from rest_framework.decorators import api_view
+from .models import Quiz
+from .serializers import QuizSerializer
 
 # Create your views here.
+@api_view(['GET'])
+def helloAPI(request):
+    return Response('Hello World!')
+
+@api_view(['GET'])
+def randomQuiz(request, id):
+    totalQuizs = Quiz.objects.all()
+    randomQuizs = random.sample(list(totalQuizs), id)
+    serializer = QuizSerializer(randomQuizs, many=True)
+
+    return Response(serializer.data)
